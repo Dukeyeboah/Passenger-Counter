96b7a3c5d00ada2d0b97b087d9120b7b8e9d1413
Dukeyeboah <117697130+Dukeyeboah@users.noreply.github.com>
2023-04-17 13:19:51 -0500

    new function for reset button

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-//document.getElementById("count-el").innerText = 5
-
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let countDash = ""
  
-
 function increment() {
     count += 1
     countEl.textContent = count
-    //countEl.innerText = count
 }
 
 function save() {
-    console.log(count)
-}
-
-function save() {
-    let countDash = count + " - "
-    //saveEl.innerText += countDash 
-    saveEl.textContent += countDash //.textContent is preferred to .innerText so it's no longer saveEl.innetText because innerText doesn't show hidden contents, only human readable characters
+    countDash = count + " - "
+    saveEl.textContent += countDash //.textContent is preferred to .innerText so it's no longer 
     count = 0
     countEl.textContent = count
-    //countEl.textContent = 0
-    //count = 0
 }
 
-
-// console.log("button was clicked")
-// console.log(count);
+function reset(){
+    countDash = ""
+    saveEl.textContent = `Previous Entries: ` 
+    count = 0
+    countEl.textContent = count 
+}
